4e7f148bcbc36e3f8bd902c5ba813775a2c353fc
Kajal Kumari <114283323+kajalkumari2507@users.noreply.github.com>
2023-09-21 12:25:16 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBesXSIoq4FIfIP2YEsg1861HgVOrUE2CA",
-  authDomain: "shopieefinance.firebaseapp.com",
-  projectId: "shopieefinance",
-  storageBucket: "shopieefinance.appspot.com",
-  messagingSenderId: "997707463111",
-  appId: "1:997707463111:web:c20d4a71e4bda72bd0c1e2",
-  measurementId: "G-EXYYC62C7R"
+  apiKey: "AIzaSyCLvoaQ5IeXTpRk9O_1EBKWZ5j1uucU7k4",
+  authDomain: "financetracker-af28a.firebaseapp.com",
+  projectId: "financetracker-af28a",
+  storageBucket: "financetracker-af28a.appspot.com",
+  messagingSenderId: "435916393103",
+  appId: "1:435916393103:web:8bd1eb5b6c2d6a5c7e7efa",
+  measurementId: "G-4SGLKL703M"
 };
 
 const app = initializeApp(firebaseConfig);
